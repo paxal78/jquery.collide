fb7bc11c6005272cd09eb2e38816d11b33b4963a
paxal78 <paxal78@gmail.com>
2013-10-05 18:32:29 +0200

    #

--- jquery.collide.js
+++ jquery.collide.js
@@ -1,13 +1,15 @@
 (function($) {
-    $.fn.collide = function(selector) {
-        var matchs = [];
-        var object1 = $(this);
-        $(selector).each(function() {
-            var object2 = $(this);
-            if (object1.offset().left < object2.offset().left + object2.width() && object1.offset().left + object1.width() > object2.offset().left && object1.offset().top < object2.offset().top + object2.height() && object1.offset().top + object1.height() > object2.offset().top) {
-                matchs.push(object2)
-            }
-        })
-        return matchs;
-    }
+    $.extend($.fn, {
+        collide: function(selector) {
+            var matchs = [];
+            var object1 = $(this);
+            $(selector).each(function() {
+                var object2 = $(this);
+                if (object1.offset().left < object2.offset().left + object2.width() && object1.offset().left + object1.width() > object2.offset().left && object1.offset().top < object2.offset().top + object2.height() && object1.offset().top + object1.height() > object2.offset().top) {
+                    matchs.push(object2)
+                }
+            })
+            return matchs;
+        }
+    });
 }(jQuery));
